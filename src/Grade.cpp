255e40b43c977f87c268ed3837abd05f057342d3
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-08-12 19:47:57 +0200

    cleanup

--- src/Grade.cpp
+++
@@ -1,7 +0,0 @@
-//FUNKCJA OCENIACJACA CIAGI BITOWE
-
-//tej funkcji moglo by nie byc, zostala zachowana zeby nie komplikowac kodu i zachowac ogolnosc algorytmu
-inline double grade(double arg)
-{
-	return arg;
-}
